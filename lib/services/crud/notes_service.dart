18f4e12118b468cae29e3b198789b1da94ba5cd1
HazemSayad <94578938+HazemSayad@users.noreply.github.com>
2022-07-21 01:40:23 +0300

    fixed a typo, added string constants, created fromRow()

--- lib/services/crud/notes_service.dart
+++ lib/services/crud/notes_service.dart
@@ -2,9 +2,13 @@ import 'package:path/path.dart';
 import 'package:path_provider/path_provider.dart';
 import 'package:sqflite/sqflite.dart';
 
+const idColumn = 'id';
+const textColumn = 'text';
+const isOwnerColumn = 'is_owner';
+
 class DatabaseNote {
   final int id;
-  final int text;
+  final String text;
   final bool isOwner;
 
   DatabaseNote({
@@ -12,4 +16,9 @@ class DatabaseNote {
     required this.text,
     required this.isOwner,
   });
+
+  DatabaseNote.fromRow(Map<String, Object?> map)
+      : id = map[idColumn] as int,
+        text = map[textColumn] as String,
+        isOwner = (map[isOwnerColumn] as int) == 1 ? true : false;
 }
