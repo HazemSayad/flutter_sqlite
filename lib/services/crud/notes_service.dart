1b7883e61ba98d5383f215f53377b3066b7a4f7a
HazemSayad <94578938+HazemSayad@users.noreply.github.com>
2022-07-21 05:49:32 +0300

    creating database functions

--- lib/services/crud/notes_service.dart
+++ lib/services/crud/notes_service.dart
@@ -2,9 +2,59 @@ import 'package:path/path.dart';
 import 'package:path_provider/path_provider.dart';
 import 'package:sqflite/sqflite.dart';
 
+const dbName = 'notes.db';
+const noteTable = 'notes';
+
 const idColumn = 'id';
 const textColumn = 'text';
 const isOwnerColumn = 'is_owner';
+
+const createNotesTable = '''CREATE TABLE IF NOT EXISTS "notes" (
+        "id"	INTEGER NOT NULL,
+        "text"	TEXT,
+        "is_owner"	INTEGER NOT NULL DEFAULT 0,
+        PRIMARY KEY("id" AUTOINCREMENT)
+      );''';
+
+class DatabaseAlreadyOpenException implements Exception {}
+
+class UnableToGetDocumentsDirectory implements Exception {}
+
+class DatabaseIsNotOpen implements Exception {}
+
+class NotesService {
+  Database? _db;
+
+  Future<void> close() async {
+    final db = _db;
+    if (db == null) {
+      throw DatabaseIsNotOpen();
+    } else {
+      db.close();
+      _db = null;
+    }
+  }
+
+  Future<void> open() async {
+    if (_db != null) {
+      throw DatabaseAlreadyOpenException();
+    }
+
+    try {
+      final docsPath = await getApplicationDocumentsDirectory();
+      final dbPath = join(docsPath.path, dbName);
+      final db = await openDatabase(dbPath);
+
+      _db = db;
+
+      await db.execute(createNotesTable);
+    } on MissingPlatformDirectoryException {
+      throw UnableToGetDocumentsDirectory();
+    } catch (e) {
+      print(e);
+    }
+  }
+}
 
 class DatabaseNote {
   final int id;
